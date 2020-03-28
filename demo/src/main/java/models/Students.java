1fa6d6a5355a3dd525d647cc34a4e734dadc6488
Mo Russ <mauriceruss302@gmail.com>
2020-03-27 20:55:00 -0400

    Students and Instructors classes created

---
+++ demo/src/main/java/models/Students.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Students extends People<Student> {
+    public Students(List personList) {
+        super(personList);
+    }
+
+
+    @Override
+    public Iterator<Student> iterator() {
+        return null;
+    }
+}
