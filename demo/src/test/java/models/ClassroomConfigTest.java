3a53527664261c5e510f4b1ec07d2044fc88addf
Mo Russ <mauriceruss302@gmail.com>
2020-03-31 20:28:23 -0400

    created ClassroomConfigTest class getting Caused by: org.springframework.beans.factory.BeanCreationException: Error and nested exception is org.springframework.beans.factory.NoSuchBeanDefinitionException

---
+++ demo/src/test/java/models/ClassroomConfigTest.java
@@ -0,0 +1,30 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ClassroomConfig.class)
+class ClassroomConfigTest {
+
+    @Autowired
+    @Qualifier("currentCohort")
+    Classroom currentCohort;
+
+    @Autowired
+    @Qualifier("previousCohort")
+    Classroom previousCohort;
+
+    @Test
+    void currentCohort() {
+    }
+
+    @Test
+    void previousCohort() {
+    }
+}
